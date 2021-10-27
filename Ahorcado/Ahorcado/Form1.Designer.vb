940b6f806d82bef138a78059a257a16556172f36
Jimy Diaz <jimmy.054@hotmail.com>
2021-10-26 21:57:17 -0600

    Proyecto Terminado

---
+++ Ahorcado/Ahorcado/Form1.Designer.vb
@@ -0,0 +1,243 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.NewToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ArchivoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SalirToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HelpToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.OpenFileDialog2 = New System.Windows.Forms.OpenFileDialog()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtLetra = New System.Windows.Forms.TextBox()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.MenuStrip1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.BackColor = System.Drawing.Color.Linen
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.HelpToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(1220, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.NewToolStripMenuItem, Me.ArchivoToolStripMenuItem, Me.SalirToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'NewToolStripMenuItem
+        '
+        Me.NewToolStripMenuItem.Name = "NewToolStripMenuItem"
+        Me.NewToolStripMenuItem.Size = New System.Drawing.Size(115, 22)
+        Me.NewToolStripMenuItem.Text = "New"
+        '
+        'ArchivoToolStripMenuItem
+        '
+        Me.ArchivoToolStripMenuItem.Name = "ArchivoToolStripMenuItem"
+        Me.ArchivoToolStripMenuItem.Size = New System.Drawing.Size(115, 22)
+        Me.ArchivoToolStripMenuItem.Text = "Archivo"
+        '
+        'SalirToolStripMenuItem
+        '
+        Me.SalirToolStripMenuItem.Name = "SalirToolStripMenuItem"
+        Me.SalirToolStripMenuItem.Size = New System.Drawing.Size(115, 22)
+        Me.SalirToolStripMenuItem.Text = "Salir"
+        '
+        'HelpToolStripMenuItem
+        '
+        Me.HelpToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AboutToolStripMenuItem})
+        Me.HelpToolStripMenuItem.Name = "HelpToolStripMenuItem"
+        Me.HelpToolStripMenuItem.Size = New System.Drawing.Size(44, 20)
+        Me.HelpToolStripMenuItem.Text = "Help"
+        '
+        'AboutToolStripMenuItem
+        '
+        Me.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem"
+        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(107, 22)
+        Me.AboutToolStripMenuItem.Text = "About"
+        '
+        'Button2
+        '
+        Me.Button2.Enabled = False
+        Me.Button2.FlatAppearance.BorderColor = System.Drawing.Color.Black
+        Me.Button2.FlatAppearance.BorderSize = 200
+        Me.Button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White
+        Me.Button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White
+        Me.Button2.Font = New System.Drawing.Font("Harlow Solid Italic", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.ForeColor = System.Drawing.Color.Black
+        Me.Button2.Location = New System.Drawing.Point(541, 433)
+        Me.Button2.Name = "Button2"
+        Me.Button2.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.Button2.Size = New System.Drawing.Size(202, 82)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Creador de Etiquetas"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Enabled = False
+        Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.ForeColor = System.Drawing.Color.Black
+        Me.Button3.Location = New System.Drawing.Point(745, 67)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(88, 45)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "Probar"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.Transparent
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.InitialImage = CType(resources.GetObject("PictureBox1.InitialImage"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 118)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(300, 315)
+        Me.PictureBox1.TabIndex = 4
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BackColor = System.Drawing.Color.Transparent
+        Me.PictureBox2.Location = New System.Drawing.Point(198, 239)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(81, 164)
+        Me.PictureBox2.TabIndex = 5
+        Me.PictureBox2.TabStop = False
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'OpenFileDialog2
+        '
+        Me.OpenFileDialog2.FileName = "OpenFileDialog2"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.OldLace
+        Me.Label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Label1.Font = New System.Drawing.Font("Harlow Solid Italic", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Black
+        Me.Label1.Location = New System.Drawing.Point(408, 33)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(168, 30)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "Ingrese una letra"
+        '
+        'txtLetra
+        '
+        Me.txtLetra.Enabled = False
+        Me.txtLetra.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtLetra.Location = New System.Drawing.Point(364, 74)
+        Me.txtLetra.MaxLength = 1
+        Me.txtLetra.Name = "txtLetra"
+        Me.txtLetra.Size = New System.Drawing.Size(274, 38)
+        Me.txtLetra.TabIndex = 8
+        Me.txtLetra.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'ListBox1
+        '
+        Me.ListBox1.Font = New System.Drawing.Font("Harlow Solid Italic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 20
+        Me.ListBox1.Location = New System.Drawing.Point(1052, 74)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(156, 424)
+        Me.ListBox1.TabIndex = 9
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Harlow Solid Italic", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.Black
+        Me.Label2.Location = New System.Drawing.Point(987, 29)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(233, 34)
+        Me.Label2.TabIndex = 10
+        Me.Label2.Text = "Palabras Utilizadas"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.DarkKhaki
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.ClientSize = New System.Drawing.Size(1220, 540)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.txtLetra)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.ForeColor = System.Drawing.Color.Transparent
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents NewToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents HelpToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AboutToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents ArchivoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents OpenFileDialog2 As OpenFileDialog
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtLetra As TextBox
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents SalirToolStripMenuItem As ToolStripMenuItem
+End Class
---
+++ Ahorcado/Ahorcado/Form1.vb
@@ -0,0 +1,227 @@
+﻿Public Class Form1
+    Dim palabras(14) As String
+    Dim palabra As String
+    Dim cantidad As Integer
+    Dim i As Integer
+    Dim intento As Integer = 0
+    Dim verificar As Boolean = True
+    Dim intentosExitosos As Integer = 0
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MsgBox("Bienvenido!!!")
+        palabras(0) = "hola"
+        palabras(1) = "temblor"
+        palabras(2) = "juniorexpress"
+        palabras(3) = "murcielago"
+        palabras(4) = "escombros"
+        palabras(5) = "kinalquinto"
+        palabras(6) = "informatica"
+        palabras(7) = "planta"
+        palabras(8) = "mario"
+        palabras(9) = "perro"
+        palabras(10) = "java"
+        palabras(11) = "agenda"
+        palabras(12) = "panes"
+        palabras(13) = "marco"
+        palabras(14) = "esperanza"
+
+
+        Randomize() 'Para que no agarre un patron
+        palabra = palabras(15 * Rnd())
+        'MsgBox(palabra)
+        Enabled = True 'True para activar y False para desactivar
+        If verificar = True Then
+            Button2.Enabled = True
+        End If
+
+        Button2.PerformClick()
+
+        Button2.Visible = False
+
+    End Sub
+
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim etiqueta As Label
+        Dim posetiqueta As Integer = 328 'Posicion en x
+
+        cantidad = palabra.Length
+
+        For i = 1 To cantidad Step 1
+            etiqueta = New Label()
+            etiqueta.Text = "_____"
+            etiqueta.Width = 50
+            etiqueta.Location = New Point(posetiqueta, 250)
+            Me.Controls.Add(etiqueta)
+            posetiqueta += 55
+        Next
+
+        If verificar = True Then
+            txtLetra.Enabled = True
+            Button3.Enabled = True
+
+        End If
+
+    End Sub
+
+    Private Sub NewToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NewToolStripMenuItem.Click
+        Application.Restart()
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim dibujar As Graphics = PictureBox2.CreateGraphics
+        Dim tletra As New Font("Harlow Solid Italic", 16)
+        Dim inicio As Integer = 328
+        Dim espacio As Integer = 0
+        Dim letra As Char
+        letra = LCase(txtLetra.Text)
+        Dim letraExistente As Boolean = False
+        Dim comprobar As Boolean = False
+        Dim prueba As Boolean = False
+        Dim confirmar As Integer
+
+
+
+        For i = 1 To Len(palabra) Step 1
+            If letra.Equals(GetChar(palabra, i)) Then
+                Me.CreateGraphics.DrawString(letra, tletra, Brushes.Red, (inicio + espacio), 225)
+                comprobar = True
+                letraExistente = True
+            End If
+            espacio += 55
+        Next
+
+
+        If (ListBox1.Items.Contains(LCase(txtLetra.Text))) Then
+            MsgBox("La tecla ya se utilizó")
+            comprobar = True
+            prueba = True
+        Else
+            ListBox1.Items.Add(LCase(txtLetra.Text))
+        End If
+
+
+        If (comprobar = False) Then
+            MsgBox("La letra no se encuentra en la palabra")
+            intento += 1
+            letraExistente = False
+        End If
+
+
+        If letraExistente = True Then            '
+
+            If prueba = True Then
+                confirmar = True
+            End If
+
+            If prueba = False Then
+
+
+
+                For i = 1 To Len(palabra) Step 1
+                    If letra.Equals(GetChar(palabra, i)) Then
+                        intentosExitosos += 1
+                        '  MsgBox(intentosExitosos.ToString)
+
+                    End If
+                Next
+
+
+                If (intentosExitosos.Equals(Len(palabra))) Then
+                    MsgBox("Ganó", MsgBoxStyle.Information, "Felicidades")
+                    confirmar = MsgBox("Desea volver a jugar ", vbYesNo + vbExclamation + vbDefaultButton2, "Gano")
+                    If confirmar = MsgBoxResult.Yes Then
+                        Application.Restart()
+                    Else
+                        Me.Close()
+
+                    End If
+                End If
+
+
+
+
+
+
+
+            End If
+
+        End If
+
+
+
+
+
+        If letraExistente = False Then
+            Select Case intento
+                Case 1
+                    dibujar.DrawEllipse(Pens.Black, 10, 5, 50, 50)
+                Case 2
+                    dibujar.DrawLine(Pens.Black, 35, 55, 35, 125)
+                Case 3
+                    dibujar.DrawLine(Pens.Black, 35, 80, 10, 60)
+                Case 4
+                    dibujar.DrawLine(Pens.Black, 35, 80, 55, 60)
+                Case 5
+                    dibujar.DrawLine(Pens.Black, 35, 125, 10, 145)
+                Case 6
+                    dibujar.DrawLine(Pens.Black, 35, 125, 55, 145)
+                Case 7
+                    dibujar.DrawLine(Pens.Black, 20, 60, 45, 60)
+                    MsgBox("Perdiste :(")
+
+                    MsgBox("La palabra es " + palabra)
+
+                    confirmar = MsgBox("Desea volver a  jugar ", vbYesNo + vbExclamation + vbDefaultButton2, "Perdió")
+                    If confirmar = MsgBoxResult.Yes Then
+                        Application.Restart()
+                    Else
+                        Me.Close()
+                    End If
+            End Select
+        End If
+    End Sub
+
+    Private Sub ArchivoToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ArchivoToolStripMenuItem.Click
+        Dim archivo, archivo2 As String
+        OpenFileDialog1.ShowDialog()
+        archivo = OpenFileDialog1.FileName
+        archivo2 = My.Computer.FileSystem.ReadAllText(archivo)
+    End Sub
+
+    Private Sub txtLetra_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtLetra.KeyPress
+
+        Dim tecla As Char
+        Dim letra As Boolean
+
+        If Char.IsDigit(e.KeyChar) Then
+            e.Handled = True
+        ElseIf Char.IsControl(e.KeyChar) Then
+            e.Handled = False
+        ElseIf Char.IsSymbol(e.KeyChar) Then
+            e.Handled = True
+        ElseIf Char.IsSeparator(e.KeyChar) Then
+            e.Handled = True
+        ElseIf Char.IsWhiteSpace(e.KeyChar) Then
+            e.Handled = True
+        Else
+            e.Handled = False
+            tecla = e.KeyChar.ToString
+
+            If txtLetra.Text.Contains(tecla) Then
+                MsgBox("la tecla ya se ingreso ")
+                letra = True
+                e.Handled = True
+            End If
+        End If
+
+
+
+    End Sub
+
+    Private Sub SalirToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SalirToolStripMenuItem.Click
+        Me.Close()
+
+    End Sub
+End Class
